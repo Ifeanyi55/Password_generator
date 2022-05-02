b291a720199ccbb9a60bc20287773fd98ce9a1f6
Ifeanyi <72707830+Ifeanyi55@users.noreply.github.com>
2022-05-02 14:12:16 +0100

    An alphanumeric password generating script

---
+++ Password.R
@@ -0,0 +1,21 @@
+passwordy = function(password_length){
+  
+  
+  scramble = sample(LETTERS)
+  numbers = sample(0:9)
+  scramble_numbers = paste(scramble,numbers)
+  shuffle = sample(scramble_numbers)
+  paste_shuffle = paste(shuffle,collapse = " ")
+  unlist_shuffle = unlist(c(strsplit(paste_shuffle,split = " ")))
+  new_shuffle = sample(unlist_shuffle)
+  paste_new_shuffle = paste(new_shuffle,collapse = " ")
+  unlist_new_shuffle = unlist(c(strsplit(paste_new_shuffle,split = " ")))
+  shuffle_again = sample(unlist_new_shuffle)
+  head_shuffle_again = head(shuffle_again,password_length)
+  one_more_shuffle = sample(head_shuffle_again)
+  paste_final = paste(one_more_shuffle,collapse = " ")
+  
+  return(paste_final)
+  
+}
+
